94c9f89448107b690ab4c2122c3f46cad759e796
Matthew Kirk <matthew.kirk@roma1.infn.it>
2021-06-07 11:59:05 +0200

    Add failure option to bash script

--- bash/simple_script.sh
+++ bash/simple_script.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
-echo "Hello world"
+if [[ -n $1 ]]
+then
+    arg="$1"
+    if [[ "$arg" = "bad" ]]
+    then
+        echo "Bad argument"
+        exit 1
+    fi
+else
+    arg="world"
+fi
+
+echo "Hello $arg"
 echo "Goodbye"
 
